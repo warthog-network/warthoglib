cfb9e706e55a91031422989a1c8f7a9e045bee6a
JulsKawa <JulsKawa>
2025-10-23 10:13:21 +0200

    feat : toArray() method, it's needed to build tx.

--- src/main/java/com/warthog/lib/Transaction.java
+++ src/main/java/com/warthog/lib/Transaction.java
@@ -1,6 +1,7 @@
 package com.warthog.lib;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import org.bouncycastle.util.encoders.Hex;
 
 public class Transaction {
@@ -8,33 +9,37 @@ public class Transaction {
     String pinHash;
     int pinHeight;
     int nonceId;
-    int amountE8;
-    int feeE8;
+    long amountE8;
+    long feeE8;
     String to;
 
     // TODO : Maybe add more check ??
+    /*
     public Transaction() {
-        if (this.pinHash.length() != 64 && this.pinHash == null) {
+        if (this.pinHash.length() != 64 && this.pinHash != null) {
             throw new HandledException(
                 "BAD_PINHASH",
                 "pinHash haven't an length of 64"
             );
         }
     }
+*/
 
-    public static byte[] convertIntToByteArray(int value, int size) {
-        return ByteBuffer.allocate(size).putInt(value).array();
-    }
-
-    public byte[] toHex() {
-        var toc = ByteBuffer.wrap(Hex.decode(this.to)).slice(0, 20);
-        ByteBuffer tx = ByteBuffer.wrap(Hex.decode(this.pinHash));
-        tx.put(convertIntToByteArray(pinHeight, 4));
-        tx.put(convertIntToByteArray(nonceId, 4));
-        tx.put(new byte[] { 0, 0, 0, 0 });
-        tx.put(convertIntToByteArray(feeE8, 8));
-        tx.put(toc);
-        tx.put(convertIntToByteArray(amountE8, 8));
+    public byte[] toArray() {
+        byte[] pinHashBytes = Hex.decode(this.pinHash);
+        byte[] toAddrBytes = Hex.decode(this.to);
+        int totalSize = pinHashBytes.length + 4 + 4 + 3 + 8 + 20 + 8;
+        ByteBuffer tx = ByteBuffer.allocate(totalSize);
+        tx.order(ByteOrder.BIG_ENDIAN);
+        tx.put(pinHashBytes);
+        tx.putInt(pinHeight);
+        tx.putInt(nonceId);
+        tx.put((byte) 0);
+        tx.put((byte) 0);
+        tx.put((byte) 0);
+        tx.putLong(feeE8);
+        tx.put(toAddrBytes, 0, 20);
+        tx.putLong(amountE8);
         return tx.array();
     }
 }
--- src/test/java/com/warthog/lib/AppTest.java
+++ src/test/java/com/warthog/lib/AppTest.java
@@ -2,6 +2,7 @@ package com.warthog.lib;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.bouncycastle.util.encoders.Hex;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -24,5 +25,33 @@ public class AppTest {
         );
         var addr = "3661579d61abde5837a8686dc4d65348a2fc61b1fe5f4093";
         assertTrue(addr.equals(acc.address));
+    }
+
+    @Test
+    public void TestTxBuilding() {
+        /*
+        pinHash = "35af90b5c89e176bde16260db20a57dde97392c1d2bf89474ca63f8cd45aa4ad"
+        pinHeight = 3643776
+        nonceId = 0
+        feeE8 = 9992
+        toAddr = "0000000000000000000000000000000000000000de47c9b2"
+        amountE8 = 100000000
+        byte = b"5\xaf\x90\xb5\xc8\x9e\x17k\xde\x16&\r\xb2\nW\xdd\xe9s\x92\xc1\xd2\xbf\x89GL\xa6?\x8c\xd4Z\xa4\xad\x007\x99\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x05\xf5\xe1\x00"
+        enco = "35af90b5c89e176bde16260db20a57dde97392c1d2bf89474ca63f8cd45aa4ad0037998000000000000000000000000000270800000000000000000000000000000000000000000000000005f5e100"
+        enco2= "35af90b5c89e176bde16260db20a57dde97392c1d2bf89474ca63f8cd45aa4ad00379980000000000000000000000000000000000000000000000000000000de47c9b2000000000000270800000000000000000000000000000000000000000000000005f5e10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+        */
+        var pyver =
+            "35af90b5c89e176bde16260db20a57dde97392c1d2bf89474ca63f8cd45aa4ad0037998000000000000000000000000000270800000000000000000000000000000000000000000000000005f5e100";
+        var tx = new Transaction();
+        tx.pinHash =
+            "35af90b5c89e176bde16260db20a57dde97392c1d2bf89474ca63f8cd45aa4ad";
+        tx.pinHeight = 3643776;
+        tx.nonceId = 0;
+        tx.feeE8 = 9992;
+        tx.to = "0000000000000000000000000000000000000000de47c9b2";
+        tx.amountE8 = 100000000;
+
+        var finishedTx = Hex.toHexString(tx.toArray());
+        assertTrue(pyver.equals(finishedTx));
     }
 }
